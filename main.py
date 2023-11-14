c22cecfae6bb27b29b0212abf8fb3ea28d3d0005
Francokab <francoiskabiry@yahoo.fr>
2023-11-14 22:11:41 +0100

    added some comment to aid on the implementation of construct_CT

--- main.py
+++ main.py
@@ -2,7 +2,19 @@ import numpy as np
 from dotGraphRenderer import renderDotGraph
 from petriNetsClasses import PetriNet
 
-
+def construct_CT(PT: PetriNet):
+    pass
+    # Initialize tree
+    # Initialize unprocessed
+    # while unprocessed is not null
+        # select an element M1 of unprocessed
+        # if the element is not already a processed element of the tree
+            # for each possible transition from this element
+                # add the new element (M2) to the tree and to unprocessed
+                # if there is a path between M3 -> M1 with M3 < M2
+                    # if M3[p] < M2[i] then M2[i] = inf
+            # remove the element M1 from unprocessed
+    # return the tree
 
 def main():
     Wpt = np.array([[1, 0, 0, 0],
