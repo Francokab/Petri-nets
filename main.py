6e6db4677ed4542e62acc3e7b43569c84208a677
Francokab <francoiskabiry@yahoo.fr>
2023-12-11 23:01:14 +0100

    added a third createPT function

--- main.py
+++ main.py
@@ -10,60 +10,85 @@ def createPT1():
                     [0, 1, 0, 0],
                     [0, 0, 0, 1],
                     [0, 0, 0, 1],
-                    [0, 0, 0, 0],],dtype=int)
+                    [0, 0, 0, 0],], dtype=int)
 
     Wtp = np.array([[0, 1, 1, 1, 0, 0],
                     [1, 0, 0, 0, 0, 0],
                     [0, 0, 0, 0, 1, 0],
-                    [0, 0, 0, 0, 1, 1],],dtype=int)
+                    [0, 0, 0, 0, 1, 1],], dtype=int)
 
     M0 = np.array([1, 1, 0, 0, 0, 0])
-    
+
     I = np.array([[-1, -1, -1, -1],
                   [-1, -1, -1, -1],
                   [-1, -1, -1, -1],
                   [-1, -1, -1, -1],
                   [-1, -1, -1, -1],
-                  [-1, -1, -1, -1]],dtype=int)
-    
-    PT = PetriNet(Wpt,Wtp,M0, I)
-    
+                  [-1, -1, -1, -1]], dtype=int)
+
+    PT = PetriNet(Wpt, Wtp, M0, I)
+
     return PT
+
 
 def createPT2():
     Wpt = np.array([[0, 0, 2, 0],
                     [0, 0, 0, 1],
-                    [0, 0, 0, 0],],dtype=int)
-    
+                    [0, 0, 0, 0],], dtype=int)
+
     Wtp = np.array([[0, 0, 0],
                     [0, 0, 0],
                     [0, 0, 1],
-                    [1, 0, 0],],dtype=int)
-    
-    M0 = np.array([0,7,0])
-    
+                    [1, 0, 0],], dtype=int)
+
+    M0 = np.array([0, 7, 0])
+
     I = np.array([[0, 5, -1, -1],
                   [-1, -1, -1, -1],
-                  [-1, -1, -1, -1],],dtype=int)
-    
+                  [-1, -1, -1, -1],], dtype=int)
 
-    PT = PetriNet(Wpt,Wtp,M0, I)
+    PT = PetriNet(Wpt, Wtp, M0, I)
     return PT
+
+
+def createPT3():
+    Wpt = np.array([[1, 0, 0, 0],
+                    [0, 1, 0, 0],
+                    [1, 0, 1, 0],
+                    [0, 1, 0, 1],], dtype=int)
+
+    Wtp = np.array([[0, 1, 1, 0],
+                    [2, 0, 0, 1],
+                    [0, 0, 0, 1],
+                    [0, 0, 1, 0],], dtype=int)
+
+    M0 = np.array([1, 0, 1, 0])
+
+    I = np.array([[-1, -1,  0, -1],
+                  [-1, -1, -1,  0],
+                  [-1, -1, -1, -1],
+                  [-1, -1, -1, -1],], dtype=int)
+
+    PT = PetriNet(Wpt, Wtp, M0, I)
+    return PT
+
 
 def main():
 
-    PTI = createPT1()
+    PTI = createPT2()
     render_dot_graph(create_dot_graph_petrinet(PTI), "Petri_Net_inhib")
-    PT = PTI.PTI_to_PT(6)
+    PT = PTI.PTI_to_PT(7)
     render_dot_graph(create_dot_graph_petrinet(PT), "Petri_Net")
-    
-    tree = PT.construct_CT_with_bounds(0, PTI)
-    render_dot_graph(create_dot_graph_tree_with_id(tree), "Converability_Tree")
-    
+
+    tree_PTI = PTI.construct_CT()
+    tree_PT = PT.construct_CT_with_bounds(0, PTI)
+    render_dot_graph(create_dot_graph_tree_with_id(tree_PTI), "Converability_Tree_PTI")
+    render_dot_graph(create_dot_graph_tree_with_id(tree_PT), "Converability_Tree_PT")
+
     # tree = PT.construct_CT()
     # render_dot_graph(create_dot_graph_petrinet(PT), "Petri_Net")
     # render_dot_graph(create_dot_graph_tree_with_id(tree), "Converability_Tree")
 
+
 if __name__ == "__main__":
     main()
-
